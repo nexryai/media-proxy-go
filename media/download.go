f5e7ce67358581f9be8bf5a055e68b907bd9d656
nexryai <git@nexryai.online>
2023-06-30 09:26:41 +0900

    ダウンロードサイズ制限を30MBに

--- media/download.go
+++ media/download.go
@@ -10,14 +10,22 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"strconv"
 )
+
+type limitedReader struct {
+	rc io.ReadCloser
+	n  int64
+}
 
 // URLを取得して、リーダーを返す
 func fetchImage(url string) (*bytes.Buffer, string, error) {
 	core.MsgDebug(fmt.Sprintf("Download image: %s", url))
 
-	resp, err := http.Get(url)
+	// 現状では30MBに制限しているが変えられるようにするべきかも
+	maxSize := int64(30 * 1024 * 1024)
+
+	resp, err := downloadFile(url, maxSize)
 	if err != nil {
 		return nil, "", fmt.Errorf("failed to fetch image: %v", err)
 	}
@@ -49,17 +57,40 @@ func fetchImage(url string) (*bytes.Buffer, string, error) {
 	return imageBuffer, contentType, nil
 }
 
-func saveResponseToFile(resp *http.Response, filename string) error {
-	file, err := os.Create(filename)
+func downloadFile(url string, maxSize int64) (*http.Response, error) {
+	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("failed to create file: %v", err)
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to save response to file: %v", err)
+		return nil, err
 	}
 
-	return nil
+	contentLength := resp.Header.Get("Content-Length")
+	if contentLength != "" {
+		length, err := strconv.ParseInt(contentLength, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		if length > maxSize {
+			resp.Body.Close()
+			return nil, fmt.Errorf("file size exceeds the limit")
+		}
+	}
+
+	resp.Body = &limitedReader{rc: resp.Body, n: maxSize}
+	return resp, nil
+}
+
+func (lr *limitedReader) Read(p []byte) (int, error) {
+	if lr.n <= 0 {
+		return 0, io.EOF
+	}
+	if int64(len(p)) > lr.n {
+		p = p[:lr.n]
+	}
+	n, err := lr.rc.Read(p)
+	lr.n -= int64(n)
+	return n, err
+}
+
+func (lr *limitedReader) Close() error {
+	return lr.rc.Close()
 }
