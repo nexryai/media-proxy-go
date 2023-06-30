6994ab934ab969f70fc3cdce36faa029f87b4d53
scil-t <torrca@torrca.com>
2023-06-30 12:12:06 +0900

    環境変数からポートの取得

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"git.sda1.net/media-proxy-go/core"
 	"git.sda1.net/media-proxy-go/server"
 	"github.com/pkg/profile"
@@ -14,10 +16,28 @@ func createServer(listenPort string) {
 	core.ExitOnError(err, "Failed to create server")
 }
 
+func getPort() (int) {
+	port := os.Getenv("PORT")
+	const defaultPort = 8080
+
+	if port == "" {
+		return defaultPort
+	}
+
+	portNum, err := strconv.Atoi(port)
+	if err != nil {
+		core.MsgErr("PORT '%s' is not valid\n", port)
+		return defaultPort
+	}
+	return portNum
+
+}
+
 func main() {
 	if core.IsDebugMode() {
 		defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
 	}
 	core.MsgInfo("Starting media-proxy-go ...")
-	createServer("8080")
+	port := getPort()
+	createServer(port)
 }
