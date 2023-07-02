465331ee4ba8ec0ab258dc5f6f842ddab530468c
nexryai <git@nexryai.online>
2023-07-02 21:00:31 +0900

    Fix: プレビューサムネイル生成時の比率がおかしい問題を修正

--- media/proxy.go
+++ media/proxy.go
@@ -67,7 +67,28 @@ func ProxyImage(url string, widthLimit int, heightLimit int, isStatic bool) (*[]
 		// widthLimitかheightLimitを超えている場合のみ処理する
 		core.MsgDebug(fmt.Sprintf("widthLimit: %d  heightLimit: %d", widthLimit, heightLimit))
 
-		if (*img).Bounds().Dx() > widthLimit || (*img).Bounds().Dy() > heightLimit {
+		imgWidth := (*img).Bounds().Dx()
+		imgHeight := (*img).Bounds().Dy()
+
+		if imgWidth > widthLimit || imgHeight > heightLimit {
+
+			// 超過量を算出
+			widthExcess := imgWidth - widthLimit
+			heightExcess := imgHeight - heightLimit
+
+			// widthLimitとheightLimitが両方超過してる場合、超過している部分が少ない方のlimitは0にして比率を維持する
+			if imgWidth > widthLimit && imgHeight > heightLimit {
+				if widthExcess < heightExcess {
+					widthLimit = 0
+				} else {
+					heightLimit = 0
+				}
+			} else if imgWidth > widthLimit {
+				widthLimit = 0
+			} else {
+				heightLimit = 0
+			}
+
 			resizedImg := resizeImage(img, widthLimit, heightLimit)
 			img = &resizedImg
 		}
