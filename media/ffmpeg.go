233df0e42b5d0ca84c406947efd3cd11ae07e05c
nexryai <nexryai@sda1.net>
2023-11-23 13:31:27 +0900

    オリジナル画像の縦高さが偶数じゃないとエラーになる不具合の修正

--- media/ffmpeg.go
+++ media/ffmpeg.go
@@ -24,7 +24,7 @@ func convertWithFfmpeg(opts *ffmpegOpts) (*[]byte, error) {
 		if opts.height%2 != 0 {
 			opts.height -= 1
 		}
-		ffmpegArgs = append(ffmpegArgs, "-vf", fmt.Sprintf("scale=%d*dar:%d", opts.height, opts.height))
+		ffmpegArgs = append(ffmpegArgs, "-vf", fmt.Sprintf("scale=-2:%d", opts.height))
 	}
 
 	ffmpegArgs = append(ffmpegArgs, "-loop", "0", "-crf", strconv.Itoa(int(opts.ffmpegCrf)), "-f", opts.targetFormat, tmpFilePath)
