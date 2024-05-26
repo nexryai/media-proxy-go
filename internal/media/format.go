4426213634c3ed52d34497bde3909a930b23e874
nexryai <nexryai@sda1.net>
2024-05-26 14:29:02 +0900

    Add isUsesColorPalette

--- internal/media/format.go
+++ internal/media/format.go
@@ -4,24 +4,28 @@ import (
 	"bytes"
 )
 
-func isAnimatedPNG(imageData *[]byte) bool {
+func isAnimatedPNG(data *[]byte) bool {
 	// PNGヘッダーをチェック
-	isPNG := bytes.HasPrefix(*imageData, []byte("\x89PNG\r\n\x1a\n"))
+	isPNG := bytes.HasPrefix(*data, []byte("\x89PNG\r\n\x1a\n"))
 	if !isPNG {
 		return false
 	}
 
 	// APNGのシグネチャをチェック
-	isAPNG := bytes.Contains(*imageData, []byte("acTL"))
-	return isAPNG
+	return len(*data) > 41 && string((*data)[37:41]) == "acTL"
 }
 
-func isAnimatedWebP(imageData *[]byte) bool {
+func isAnimatedWebP(data *[]byte) bool {
 	// ヘッダーをチェック
 	// Animated WebPの場合、ファイルの0x1Eから0x22がANIMになる
-	return string((*imageData)[0x1E:0x22]) == "ANIM"
+	return len(*data) > 0x22 && string((*data)[0x1E:0x22]) == "ANIM"
 }
 
 func isAVIF(data *[]byte) bool {
 	return len(*data) > 12 && string((*data)[4:12]) == "ftypavif"
+}
+
+func isUsesColorPalette(data *[]byte) bool {
+	// PNGの場合はPLTEチャンクが存在するかで判断
+	return len(*data) > 64 && string((*data)[60:64]) == "PLTE"
 }
--- internal/media/format_test.go
+++ internal/media/format_test.go
@@ -25,6 +25,7 @@ func TestIsAnimatedWebP(t *testing.T) {
 		t.Errorf("isAnimatedPng returned incorrect results")
 	}
 }
+
 func TestIsAVIF(t *testing.T) {
 	imageBufferPtr, downloaderDetectedType, err := fetchImage("https://s3.sda1.net/nyan/contents/bc0701f3-6a5e-471e-ae35-ddfae7d0b7f6.avif")
 	if err != nil {
@@ -37,5 +38,27 @@ func TestIsAVIF(t *testing.T) {
 
 	if downloaderDetectedType != "image/avif" {
 		t.Errorf("fetchImage returned incorrect results")
+	}
+}
+
+func TestIsUsesColorPalette(t *testing.T) {
+	// 使ってない
+	imageBufferPtr, _, err := fetchImage("https://apng.onevcat.com/assets/elephant.png")
+	if err != nil {
+		t.Errorf("fetchImage returned an error: %v", err)
+	}
+
+	if isUsesColorPalette(imageBufferPtr) {
+		t.Errorf("isUsesColorPalette returned incorrect results: should be false")
+	}
+
+	// カラーパレットを使ってる
+	imageBufferPtr, _, err = fetchImage("https://s3.sda1.net/firefish/contents/0280219c-8356-4c8c-b808-4b3e4b78dfa6.apng")
+	if err != nil {
+		t.Errorf("fetchImage returned an error: %v", err)
+	}
+
+	if !isUsesColorPalette(imageBufferPtr) {
+		t.Errorf("isUsesColorPalette returned incorrect results: should be true")
 	}
 }
